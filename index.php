5d2ae13954a0b5b50214df0a7bb887c83550f4c2
Solllux <joyce8@umbc.edu>
2014-09-27 17:15:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
 <body>
-  Hello World
+	<div class = "blur"></div>
+	<div id="text">
+		<div id="login">
+			<a class="btn-auth btn-facebook large" href="#">hello</a>
+			
+		</div>
+	</div>
+<style>
+
+	#text{
+		position: absolute;
+		top: 20%;
+		left: 20%;
+		width: 60%;
+		height: 60%;
+		background-color: #333333;
+		opacity: .4;
+		filter: alpha(opacity=40);
+		border-radius: 15px;
+		padding: 16px;
+	}
+	h1{
+		opacity: 1;
+		filter: alpha(opacity=100);
+	}
+	.blur {
+		background:url('http://technical.ly/baltimore/wp-content/uploads/sites/3/2013/10/UMBCcommons.jpg') no-repeat center;
+		background-size:cover;
+		-webkit-filter: blur(8px);
+		-moz-filter: blur(8px);
+		-o-filter: blur(8px);
+		-ms-filter: blur(8px);
+		filter: blur(8px);
+		position: fixed;
+		width: 111%;
+		height: 110%;
+		left: -10;
+		top: -10;
+	}
+	
+</style>
+
+
+<script>
+	function testing(){
+		document.write('asdf');
+	}
+	function show(ID){
+		document.write('asdf');
+		var a;
+		for(a = 0; a < document.getElementsByTagName("li").length; a++){
+			if(inputs[i].id.equals(ID){
+				document.getElementsById(ID)[a].style.height ="50px";
+			}
+		}
+	}
+</script>
+
+
 </body>
